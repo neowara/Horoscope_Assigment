b89522158b86917c20e2c46378af4023e3c1a6fe
neowara <32764514+neowara@users.noreply.github.com>
2018-03-19 23:30:11 +0100

    Final PHP code
    Add final PHP code to the page to make the API work

--- deleteHoroscope.php
+++ deleteHoroscope.php
@@ -1,6 +1,9 @@
-<?php 
-
-
-
-
+<?php
+    session_start();
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+        if(isset($_SESSION["myHoroscope"])){
+            unset($_SESSION["myHoroscope"]);
+        }
+    }
 ?>
--- viewHoroscope.php
+++ viewHoroscope.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
+    session_start();
 
-
-
-
+     if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+        if (isset($_SESSION["myHoroscope"])){
+            echo $_SESSION["myHoroscope"];
+        }
+    }
 ?>
